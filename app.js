58c1862bc98dc7cb8b4e3b7a399034d13331198f
Kesraoui Mohamed <92538732+KesraouiMohamed1991@users.noreply.github.com>
2023-12-21 19:00:08 +0100

    yes

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 require('dotenv').config();
 
-require('./models/connection')
 var express = require('express');
+require('./model/connexion')
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
 var app = express();
 const cors = require('cors');
 app.use(cors());
-const fileUpload = require('express-fileupload');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(fileUpload());
-app.use('/bars', usersRouter);
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 module.exports = app;
--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.CONNECTION_STRING;
+let CONNECTION_STRING = 'mongodb+srv://kesraoui15003:harlembynight@cluster0.unytmwf.mongodb.net/MarseilleSips'
+
+const connectionString = CONNECTION_STRING;
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
   .catch(error => console.error(error));
